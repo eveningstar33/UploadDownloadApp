46fe73e2d7d52d08e36f6c97196b4d7fb22c6cf2
Dragos Sarpe <contact@usdgadget.com>
2018-11-27 19:50:03 +0200

    Implemented the Upload method

--- src/main/java/com/dgs/document/documentweb/controllers/DocumentController.java
+++ src/main/java/com/dgs/document/documentweb/controllers/DocumentController.java
@@ -1,13 +1,40 @@
 package com.dgs.document.documentweb.controllers;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.dgs.document.documentweb.entities.Document;
+import com.dgs.document.documentweb.repos.DocumentRepository;
 
 @Controller
 public class DocumentController {
+	
+	@Autowired
+	private DocumentRepository repository;
 
 	@RequestMapping("/displayUpload")
 	public String displayUpload() {
+		return "documentUpload";
+	}
+	
+	@PostMapping("/upload")
+	public String uploadDocument(@RequestParam("document") MultipartFile multipartFile, 
+			@RequestParam("id") long id ) {
+		Document document = new Document();
+		document.setId(id);
+		document.setName(multipartFile.getName()); 
+		try {
+			document.setData(multipartFile.getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		repository.save(document); 
 		return "documentUpload";
 	}
 }
